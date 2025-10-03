2e84acf1e9d6bf4309b8c5db559d8213ffd29edd
Neha Mundhra <135490586+Nehaa1727@users.noreply.github.com>
2025-10-03 17:54:37 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,45 +1,3 @@
-// import React from 'react'
-// import Navbar from './components/Navbar/Navbar'
-// import Sidebar from './components/Sidebar/Sidebar'
-// import { Route, Routes } from 'react-router-dom'
-// import Add from './pages/Add/Add'
-// import List from './pages/List/List'
-// import Orders from './pages/Orders/Orders'
-// import { ToastContainer} from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css'
-// import Header from './components/Header/Header'
-
-// const App = () => {
-
-//       const url = "http://localhost:4000"
-
-//   return (
-//     <div>
-//       <ToastContainer/>
-//       <Navbar/>
-//       <hr/>
-//       <div className="app-content">
-//         <Sidebar/>
-//         <Header/>
-//         <Routes>
-//           <Route path='/add' element={<Add url={url}/>}/>
-//           <Route path='/list' element={<List url={url}/>}/>
-//           <Route path='/orders' element={<Orders url={url}/>}/>
-//         </Routes>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -52,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header/Header'
 
 const App = () => {
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-backend-eqt3.onrender.com/"
   const location = useLocation();
 
   // pages where header should be visible
